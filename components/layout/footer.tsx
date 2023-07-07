d2a4e6a1377f61d7ef38419cfe7c5f75ea6fb0d7
Adilson Bacelar <a.m.bacelar@gmail.com>
2023-07-07 22:28:27 +0100

    minimise footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -6,12 +6,9 @@ import VercelIcon from 'components/icons/vercel';
 import { getMenu } from 'lib/shopify';
 import { Menu } from 'lib/shopify/types';
 
-const { SITE_NAME } = process.env;
-
 export default async function Footer() {
   const currentYear = new Date().getFullYear();
   const copyrightDate = 2023 + (currentYear > 2023 ? `-${currentYear}` : '');
-  const menu = getMenu();
 
   return (
     <footer className="border-t border-gray-700 bg-white text-black dark:bg-black dark:text-white">
@@ -22,25 +19,9 @@ export default async function Footer() {
               <span className="mr-2">
                 <LogoIcon className="h-8" />
               </span>
-              <span>{SITE_NAME}</span>
+              <span>{copyrightDate}</span>
             </a>
           </div>
-          {menu.length ? (
-            <nav className="col-span-1 lg:col-span-7">
-              <ul className="grid md:grid-flow-col md:grid-cols-3 md:grid-rows-4">
-                {menu.map((item: Menu) => (
-                  <li key={item.title} className="py-3 md:py-0 md:pb-4">
-                    <Link
-                      href={item.path}
-                      className="text-gray-800 transition duration-150 ease-in-out hover:text-gray-300 dark:text-gray-100"
-                    >
-                      {item.title}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </nav>
-          ) : null}
         </div>
       </div>
     </footer>
