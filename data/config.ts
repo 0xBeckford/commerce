ce89cf75294349037d043118c18d184a2e2c90f1
Beckford <106078373+0xBeckford@users.noreply.github.com>
2023-07-07 22:07:01 +0100

    henley beckford add

--- data/config.ts
+++ data/config.ts
@@ -1,1 +1,1 @@
-export const threeFeaturedSpotlightHandles = ['the-code', 'oliver-cheshire', 'the-code'];
+export const threeFeaturedSpotlightHandles = ['the-code', 'oliver-cheshire', 'henley-beckford'];
