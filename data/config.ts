7cb18c8cee97292368433157357664b0cea51fb9
Beckford <106078373+0xBeckford@users.noreply.github.com>
2023-07-08 17:09:38 +0100

    feature profile paige

--- data/config.ts
+++ data/config.ts
@@ -1,1 +1,1 @@
-export const threeFeaturedSpotlightHandles = ['the-code', 'oliver-cheshire', 'henley-beckford'];
+export const threeFeaturedSpotlightHandles = ['the-code', 'oliver-cheshire', 'henley-beckford', 'paige-lilley'];
