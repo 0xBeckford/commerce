d5f1bf4730297c3f482a374069fd4e2902b076d0
Beckford <106078373+0xBeckford@users.noreply.github.com>
2023-07-08 19:42:15 +0100

    grey gallery

--- components/product/gallery.tsx
+++ components/product/gallery.tsx
@@ -39,7 +39,7 @@ export function Gallery({
             height={600}
             isInteractive={false}
             priority={true}
-            background="dark-grey"
+            background="grey"
             labels={{
               title,
               badgeText
@@ -83,7 +83,7 @@ export function Gallery({
                   src={image.src}
                   width={600}
                   height={600}
-                  background="dark-grey"
+                  background="grey"
                   active={isActive}
                 />
               </button>
